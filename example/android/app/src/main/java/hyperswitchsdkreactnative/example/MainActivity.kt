f9b9468e320cba28a43c53699ae095234eab3c24
sh-iv-am <shivam.shashank@juspay.in>
2025-10-14 06:46:41 +0530

    chore: removed hardcoded keys

--- example/android/app/src/main/java/hyperswitchsdkreactnative/example/MainActivity.kt
+++ example/android/app/src/main/java/hyperswitchsdkreactnative/example/MainActivity.kt
@@ -18,23 +18,6 @@ class MainActivity : ReactActivity() {
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
-  override fun createReactActivityDelegate(): ReactActivityDelegate {
-    return object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {
-      // override fun getLaunchOptions(): Bundle? {
-
-      //   val propsBundle = Bundle().apply {
-      //     putString("type", "payment")
-      //     putString("from", "rn")
-      //     putString("publishableKey",  "pk_snd_3b33cd9404234113804aa1accaabe22f" )
-      //     putString("clientSecret",  "pay_mMPDZBs0hJD9IjfVB5Ck_secret_Xay7LpZRwJnKA7fk8D1F")
-      //   }
-
-      //   val bundle = Bundle().apply {
-      //     putBundle("props", propsBundle)
-      //   }
-
-      //   return bundle
-      // }
-    }
-  }
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }

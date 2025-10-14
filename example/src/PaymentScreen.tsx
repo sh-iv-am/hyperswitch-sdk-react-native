6b7955edd2b0bfaa10a6e0764ed25a0c6b51c7f0
sh-iv-am <shivam.shashank@juspay.in>
2025-10-14 06:44:08 +0530

    chore: example app refactor

--- example/src/PaymentScreen.tsx
+++ example/src/PaymentScreen.tsx
@@ -1,136 +1,83 @@
 import { useState, useEffect, useCallback } from 'react';
-import {
-  View,
-  Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import {
   useHyper,
   type InitPaymentSessionParams,
   type InitPaymentSessionResult,
-  type PresentPaymentSheetParams,
   type PresentPaymentSheetResult,
 } from 'hyperswitch-sdk-react-native';
+import {
+  initialBaseUrl,
+  getCustomisationOptions,
+  getStatus,
+  getErrorMessage,
+} from './utils';
+import { styles } from './styles';
 
 export default function PaymentScreen() {
   const { initPaymentSession, presentPaymentSheet } = useHyper();
   const [status, setStatus] = useState<string | null | undefined>(null);
   const [message, setMessage] = useState<string | null | undefined>(null);
-  const [baseURL, setBaseURL] = useState<string>(
-    Platform.OS === 'android' ? 'http://10.0.2.2:5252' : 'http://localhost:5252'
-  );
-  const createPaymentIntent = useCallback(async (): Promise<
-    string | undefined
-  > => {
-    try {
-      const response = await fetch(`${baseURL}/create-payment-intent`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-      });
+  const [baseURL, setBaseURL] = useState<string>(initialBaseUrl);
 
-      const data = await response.json();
-
-      if (!response.ok) {
-        throw new Error(data.error || 'Failed to create payment intent');
-      }
-
-      return data.client_secret;
-    } catch (error) {
-      console.error('Error creating payment intent:', error);
-      return undefined;
-      // throw error;
+  const createPaymentIntent = useCallback(async (): Promise<string> => {
+    const response = await fetch(`${baseURL}/create-payment-intent`);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || 'Failed to create payment intent');
     }
+    return data.clientSecret;
   }, [baseURL]);
-  const setup = useCallback(async (): Promise<void> => {
-    try {
-      const paymentIntent = await createPaymentIntent();
 
+  const setup = useCallback(async (): Promise<void> => {
+    const paymentIntent = await createPaymentIntent();
+
+    if (paymentIntent !== undefined) {
       const params: InitPaymentSessionParams = {
         paymentIntentClientSecret: paymentIntent,
       };
-
       const result: InitPaymentSessionResult = await initPaymentSession(params);
-
       if (result.error) {
-        setStatus(`Initialization failed: ${result.error}`);
-        console.error('Payment session initialization failed:', result.error);
+        throw new Error(result.error);
       } else {
-        setMessage('');
         setStatus('Ready to checkout');
+        setMessage(null);
       }
-    } catch (error) {
-      console.error('Setup failed:', error);
     }
   }, [initPaymentSession, createPaymentIntent]);
-  useEffect(() => {
-    setup();
-  }, [setup]);
 
   const checkout = async (): Promise<void> => {
     try {
-      const options: PresentPaymentSheetParams = {
-        appearance: {
-          theme: 'Dark',
-          // colors: {
-          //   background: '#452061',
-          //   componentBackground: 'black',
-          //   componentText: 'white',
-          //   primary: '#77DF95',
-          //   primaryText: 'white',
-          // },
-          // primaryButton: {
-          //   shapes: {
-          //     borderRadius: 36,
-          //     shadow: {
-          //       color: '#378C46',
-          //       opacity: 0.5,
-          //       blurRadius: 10,
-          //       offset: {
-          //         x: 0,
-          //         y: 4,
-          //       },
-          //     },
-          //   },
-          // },
-          // shapes: {
-          //   shadow: {
-          //     color: '#378C46',
-          //     opacity: 1,
-          //     blurRadius: 10,
-          //     offset: {
-          //       x: 0,
-          //       y: 6,
-          //     },
-          //   },
-          // },
-        },
-        // primaryButtonLabel: 'Complete Purchase',
-      };
-
-      const result: PresentPaymentSheetResult =
-        await presentPaymentSheet(options);
-      let { error, paymentResult } = result;
+      const { error, paymentResult }: PresentPaymentSheetResult =
+        await presentPaymentSheet(getCustomisationOptions());
       if (error) {
-        console.error('Payment failed:', JSON.stringify(error));
-        setStatus(`Payment failed: ${JSON.stringify(error)}`);
+        console.error('Payment failed:', JSON.stringify(error, null, 2));
+        setStatus(`Payment failed: ${error.code}`);
+        setMessage(error.message);
       } else {
-        setStatus(paymentResult?.status);
-        setMessage(`${paymentResult?.message}`);
-        console.log('Payment completed with status:', paymentResult?.status);
-        console.log('Message:', paymentResult?.message);
+        console.log(
+          'Payment completed with status:',
+          JSON.stringify(paymentResult, null, 2)
+        );
+        setStatus(getStatus(paymentResult?.status));
+        setMessage(paymentResult?.message);
       }
     } catch (error: any) {
-      setMessage(`${error.message}`);
-      setStatus(`Checkout failed: ${JSON.stringify(error.message)}`);
       console.error('Checkout failed:', error);
+      setStatus(`Checkout failed`);
+      setMessage(error.message);
     }
   };
+
+  useEffect(() => {
+    try {
+      setup();
+    } catch (error) {
+      console.error('Setup failed:', error);
+      setStatus('Setup Error:');
+      setMessage(getErrorMessage(error));
+    }
+  }, [setup]);
 
   return (
     <View style={styles.container}>
@@ -146,49 +93,10 @@ export default function PaymentScreen() {
       <TouchableOpacity style={styles.button} onPress={checkout}>
         <Text style={styles.buttonText}>Checkout</Text>
       </TouchableOpacity>
-      {message && <Text>{message}</Text>}
-      <Text style={styles.statusText}>{status}</Text>
+      <View style={styles.status}>
+        <Text style={styles.statusText}>{status}</Text>
+        {message && <Text style={styles.messageText}>{message}</Text>}
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  textInput: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: 48,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 12,
-    borderRadius: 8,
-    fontSize: 24,
-    paddingHorizontal: 8,
-    width: '100%',
-    gap: 20,
-  },
-  container: {
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 24,
-  },
-  statusText: {
-    marginTop: 16,
-    fontSize: 24,
-    color: 'gray',
-    textAlign: 'center',
-  },
-  button: {
-    width: '100%',
-    height: 48,
-    backgroundColor: '#0355c9ff',
-    alignItems: 'center',
-    color: 'white',
-    justifyContent: 'center',
-    borderRadius: 8,
-    marginBottom: 12,
-  },
-  buttonText: {
-    color: 'white',
-  },
-});
